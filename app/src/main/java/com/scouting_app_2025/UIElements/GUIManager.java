f9b7cc563bdcf19a2f92495541077412c2781d8f
Jonathan <117413359+JAKlein26@users.noreply.github.com>
2025-02-11 17:27:16 -0800

    Small tweaks to GUIManager and UndoStack

--- app/src/main/java/com/scouting_app_2025/UIElements/GUIManager.java
+++ app/src/main/java/com/scouting_app_2025/UIElements/GUIManager.java
@@ -1,8 +1,11 @@
 package com.scouting_app_2025.UIElements;
 
+import android.content.Context;
+
 public class GUIManager {
 
-    public GUIManager() {
+    public GUIManager(Context context) {
 
     }
+
 }
--- app/src/main/java/com/scouting_app_2025/UIElements/UndoStack.java
+++ app/src/main/java/com/scouting_app_2025/UIElements/UndoStack.java
@@ -1,7 +1,5 @@
 package com.scouting_app_2025.UIElements;
 
-import androidx.annotation.NonNull;
-
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.Stack;
@@ -11,8 +9,8 @@ public class UndoStack {
     private final Stack<String> timestamps = new Stack<String>();
     private final Stack<Integer> redoStack = new Stack<Integer>();
     private final Stack<String> redoTimestamps = new Stack<String>();
-    private HashMap<Integer, Element> allElements = new HashMap<Integer, Element>();
-    private HashMap<Element, Integer> reverseElements = new HashMap<Element, Integer>();
+    private final HashMap<Integer, Element> allElements = new HashMap<Integer, Element>();
+    private final HashMap<Element, Integer> reverseElements = new HashMap<Element, Integer>();
 
     public UndoStack() {
 
@@ -35,8 +33,8 @@ public class UndoStack {
     }
     public void redo() {
         if(redoStack.isEmpty()) return;
-        redoStack.push(inputStack.pop());
-        redoTimestamps.push(timestamps.pop());
-
+        inputStack.push(redoStack.pop());
+        timestamps.push(redoTimestamps.pop());
+        Objects.requireNonNull(allElements.get(redoStack.peek())).redo();
     }
 }
